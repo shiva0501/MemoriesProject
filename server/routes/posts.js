cf6156c88c45d2a0856fb895a831d8f4ebb118d8
unknown <shiva46574@gmail.com>
2021-12-19 13:31:14 +0530

    version4

---
+++ server/routes/posts.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import { getPosts, getPostsBySearch, getPost, createPost, updatePost, likePost, deletePost, commentPost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
+router.post('/', auth, createPost);
+router.get('/:id', getPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
+
+export default router;
